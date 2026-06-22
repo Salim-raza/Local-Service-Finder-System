f59a2472b5ebd1914fa7d692ebe0d58ffc814f7e
Salim-raza <rezasalim00011@gmail.com>
2026-06-22 23:34:04 +0600

    add create_at and update_at fields

--- locations/serializers.py
+++ locations/serializers.py
@@ -5,16 +5,16 @@ class divisionSerializers(serializers.ModelSerializer):
     class Meta:
         model = Division
         fields = ["name"]
-        read_only_fields = ["id"]
+        read_only_fields = ["create_at", "update_at"]
 
 class districtSerializers(serializers.ModelSerializer):
     class Meta:
         model = District
         fields = ["name", "division"]
-        read_only_fields = ["id"]
+        read_only_fields = ["create_at", "update_at"]
 
 class upazilaSerializers(serializers.ModelSerializer):
     class Meta:
         model = Upazila
         fields = ["name", "district"]
-        read_only_fields = ["id"]
+        read_only_fields = ["create_at", "update_at"]
