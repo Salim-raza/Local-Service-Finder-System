16c57d23e64e91f69b8890b997406a0d1163a1a4
Salim-raza <rezasalim00011@gmail.com>
2026-06-28 16:45:51 +0600

    add completed booking and pending booking filter api

--- dashboard/urls/customer_urls.py
+++ dashboard/urls/customer_urls.py
@@ -1,6 +1,9 @@
-from ..views.customer_dashboard import customer_dashboard
+from ..views.customer_dashboard import *
 from django.urls import path
 
 urlpatterns = [
-    path("customer/", customer_dashboard, name="customer_dashboard")
+    path("customer/", customer_dashboard, name="customer_dashboard"),
+    path("total_oder_list/", total_oder_list, name="total_oder_list"),
+    path("completed_booking/", completed_booking, name="completed_booking"),
+    path("pending_booking/", pending_booking, name="pending_booking"),
 ]
--- dashboard/views/customer_dashboard.py
+++ dashboard/views/customer_dashboard.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from accounts.permission import IsCustomer
-from rest_framework import status
+from accounts.serializers import UserCreateSerializers
+from booking.serializers import BookingSerializers
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
+from accounts.permission import IsCustomer
+from booking.models import Booking
+from rest_framework import status
 
 
 @api_view(["GET"])
@@ -10,3 +14,48 @@ def customer_dashboard(request):
     return Response({
         "message": "welcome to customer dashboard"
     }, status=status.HTTP_200_OK)
+    
+    
+@swagger_auto_schema(
+    method="get",
+    responses={200: UserCreateSerializers(many=True), 400: "Bad Request"},
+    operation_description="get all oder"
+)
+@api_view(["GET"])
+@permission_classes([IsCustomer])
+@authentication_classes([JWTAuthentication])
+def total_oder_list(request):
+    booking = Booking.objects.filter(user=request.user)
+    serializer = BookingSerializers(booking, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    
+@swagger_auto_schema(
+    method="get",
+    responses={200: UserCreateSerializers(many=True), 400: "Bad Request"},
+    operation_description="get all completed a oder"
+)
+@api_view(["GET"])
+@permission_classes([IsCustomer])
+@authentication_classes([JWTAuthentication])
+def completed_booking(request):
+    booking = Booking.objects.filter(user=request.user, status="completed")
+    serializer = BookingSerializers(booking, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    
+@swagger_auto_schema(
+    method="get",
+    responses={200: UserCreateSerializers(many=True), 400: "Bad Request"},
+    operation_description="get all pending account"
+)
+@api_view(["GET"])
+@permission_classes([IsCustomer])
+@authentication_classes([JWTAuthentication])
+def pending_booking(request):
+    booking = Booking.objects.filter(user=request.user, status="pending")
+    serializer = BookingSerializers(booking, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
